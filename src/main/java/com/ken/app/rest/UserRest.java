91a425bd096c12f812747895f95afe8b1fdbb8df
ken0901 <ken5260715@gmail.com>
2022-10-12 22:07:30 -0700

    Add change password and forgot password API of user

--- src/main/java/com/ken/app/rest/UserRest.java
+++ src/main/java/com/ken/app/rest/UserRest.java
@@ -14,14 +14,23 @@ import java.util.Map;
 public interface UserRest {
 
     @PostMapping(path = "/signup")
-    ResponseEntity<String> signUp(@RequestBody(required = true) Map<String, String> requestMap);
+    ResponseEntity<String> signUp(@RequestBody Map<String, String> requestMap);
 
     @PostMapping(path = "/login")
-    ResponseEntity<String> login(@RequestBody(required = true) Map<String, String> requestMap);
+    ResponseEntity<String> login(@RequestBody Map<String, String> requestMap);
 
     @GetMapping(path = "/get")
     ResponseEntity<List<UserWrapper>> getAllUser();
 
     @PostMapping(path = "/update")
-    ResponseEntity<String> update(@RequestBody(required = true) Map<String, String> requestMap);
+    ResponseEntity<String> update(@RequestBody Map<String, String> requestMap);
+
+    @GetMapping(path = "/checkToken")
+    ResponseEntity<String> checkToken();
+
+    @PostMapping(path = "/changePassword")
+    ResponseEntity<String> changePassword(@RequestBody Map<String, String> requestMap);
+
+    @PostMapping(path = "/forgotPassword")
+    ResponseEntity<String> forgotPassword(@RequestBody Map<String, String> requestMap);
 }
